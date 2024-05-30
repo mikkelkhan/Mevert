b690381b56e8e23aae3d02c7fcbbb75ba2cd4f1f
Mikkel <kashifmik@hotmail.com>
2024-05-30 19:44:57 +0530

    added if no events in some city logic in filter

--- mv_events/filter.py
+++ mv_events/filter.py
@@ -32,20 +32,30 @@ def fetch_filter():
         }
         filter_data = cursor.execute(Template(config["Query"]["fetch_all_filter"]).safe_substitute(city=city, country=country))
         filter_all= filter_data.fetchall()
+        if filter_all == []:
+            return render_template('filter.html',not_show="No one is going this city events")
+        else:
+            data = []
+            for row in filter_all:
+                data.append({'username': row[0], 'name': row[1], 'city': row[2], 'Country': row[3], 'new_city': row[5],
+                             'new_Country': row[6], 'location': row[7]})
 
-        data = []
-        for row in filter_all:
-            data.append({'username': row[0], 'name': row[1], 'city': row[2], 'Country': row[3],'new_city' : row[5],'new_Country' : row[6],'location' : row[7]})
+            random_folter = random.choice(data)
+            filter_name = random_folter.get('name')
+            filter_city = random_folter.get('city')
+            filter_Country = random_folter.get('Country')
+            filter_new_city = random_folter.get('new_city')
+            filter_new_Country = random_folter.get('new_Country')
+            filter_location = random_folter.get('location')
+            return render_template('home.html', filter_name=filter_name, filter_city=filter_city,
+                                   filter_Country=filter_Country, filter_new_city=filter_new_city,
+                                   filter_new_Country=filter_new_Country, filter_location=filter_location,
+                                   button1=button1)
 
 
-        random_folter = random.choice(data)
-        filter_name = random_folter.get('name')
-        filter_city = random_folter.get('city')
-        filter_Country = random_folter.get('Country')
-        filter_new_city = random_folter.get('new_city')
-        filter_new_Country = random_folter.get('new_Country')
-        filter_location = random_folter.get('location')
-        return render_template('home.html',filter_name=filter_name,filter_city=filter_city,filter_Country=filter_Country,filter_new_city=filter_new_city,filter_new_Country=filter_new_Country,filter_location=filter_location,button1=button1)
+
+
+
 
 
 
